3e9f590138f25ce05121d5025adc6549f971bd0c
TiunovaYana <99291404+TiunovaYana@users.noreply.github.com>
2022-05-28 01:04:23 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,25 +5,4 @@
 #include  <cstdlib>
 #include  "bst.h"
 
-BST<std::string> makeTree(const char* filename) {
-  BST<std::string> tree;
-    std::ifstream file(filename);
-    if (!file) {
-        throw std::string("File Error!!! ");
-    }
-    std::string sim = "";
-    while (!file.eof()) {
-        sim = "";
-        while (true) {
-            char simv = file.get();
-            if ((simv > 64 && simv < 91) || (simv > 96 && simv < 123)) {
-                sim += tolower(simv);
-            } else {
-                break;
-            }
-        }
-        tree.add(sim);
-    }
-    file.close();
-    return tree;
-}
+
